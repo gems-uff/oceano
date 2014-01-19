e4e34075d4937df8a09fe1d997cbfb7a6f14a750
Daniel Heraclio O M Carmo <dheraclio@gmail.com>
2014-01-19 20:01:38 -0200

    Created test to determine basic files statistics per revision. Motivated by NeoPZ project investigation.

--- core/src/test/java/br/uff/ic/oceano/core/tools/metrics/extractors/dependometer/DependometerServiceNGTest.java
+++ core/src/test/java/br/uff/ic/oceano/core/tools/metrics/extractors/dependometer/DependometerServiceNGTest.java
@@ -8,10 +8,13 @@ import br.uff.ic.oceano.core.tools.metrics.extractors.dependometer.util.Adapter;
 import br.uff.ic.oceano.util.test.AbstractNGTest;
 import br.uff.ic.oceano.core.model.Revision;
 import br.uff.ic.oceano.CppProjectsHelper;
+import br.uff.ic.oceano.core.exception.ObjetoNaoEncontradoException;
+import br.uff.ic.oceano.core.model.SoftwareProject;
 import br.uff.ic.oceano.util.file.FileUtils;
 import br.uff.ic.oceano.util.file.PathUtil;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
@@ -27,6 +30,43 @@ public class DependometerServiceNGTest extends AbstractNGTest {
      * 
      */
     @Test
+    public void testData() throws ObjetoNaoEncontradoException {
+        println("test Data");
+        SoftwareProject neopz = testConstantsCpp.getDBNeoPZProject();
+        double count = 0;
+        int countUnchagedFiles = 0;
+        int minCHanged = Integer.MAX_VALUE;
+        int maxCHanged = 0;
+        Long maxRevisionNumber = null;
+        final Set<Revision> revisions = neopz.getRevisions();
+        for (Revision revision : revisions) {
+            final Set changedFiles = revision.getChangedFiles();
+            if(changedFiles != null){
+                final int size = changedFiles.size();
+                count += size;
+                minCHanged = minCHanged > size? size:minCHanged;
+                if(maxCHanged < size){
+                    maxCHanged = size;
+                    maxRevisionNumber = revision.getNumber();
+                }
+                
+                
+                countUnchagedFiles = size == 0? countUnchagedFiles+1:countUnchagedFiles;
+            } else {
+                println("Revision "+ revision+" has no changed files.");
+                countUnchagedFiles += 1;
+            }
+        }
+        println("Average files per commit: " + count/revisions.size());
+        println("Unchanged files per commit: " + countUnchagedFiles);
+        println("Minimum changed files per commit: " + minCHanged);
+        println("Maximum changed files per commit: " + maxCHanged+" in revision number: " + maxRevisionNumber);
+    }
+    
+    /**
+     * 
+     */
+    @Test
     public void testDumpEasycountResult() {
         println("dumping easyCount");
         Revision revision = testConstantsCpp.getEasyCountRevision();
@@ -37,8 +77,8 @@ public class DependometerServiceNGTest extends AbstractNGTest {
     @Test
     public void testDumpNeopzResult() throws Exception {
         println("dumping neopz");
-        Revision revision = testConstantsCpp.getNeopzRevision();
-        //Revision revision = testConstantsCpp.checkoutNeoPzRevision(2);
+        //Revision revision = testConstantsCpp.getNeopzRevision();
+        Revision revision = testConstantsCpp.checkoutNeoPzRevision(2);
         String path = dump(revision);
         FileUtils.deleteDirectory(path);
     }
