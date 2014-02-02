63adcdf4183cf143f4c8892ffb07093ab7fdd9ac
Daniel Heraclio O M Carmo <dheraclio@gmail.com>
2014-02-02 17:37:13 -0200

    Refactored to use reflection.

--- core/src/main/java/br/uff/ic/oceano/ostra/discretizer/DiscretizerFactory.java
+++ core/src/main/java/br/uff/ic/oceano/ostra/discretizer/DiscretizerFactory.java
@@ -1,12 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.uff.ic.oceano.ostra.discretizer;
 
 import br.uff.ic.oceano.core.exception.ServiceException;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  *
@@ -14,47 +8,22 @@ import java.util.Set;
  */
 public class DiscretizerFactory {
 
-    private static final Set<Class> knownDiscretizerClasses = getKnownDiscretizerClasses();
-
-    private static Set<Class> getKnownDiscretizerClasses() {
-        Set<Class> knownDiscretizerClassesToReturn = new HashSet<Class>();
-        knownDiscretizerClassesToReturn.add(NegativePositiveDiscretizer.class);
-
-        return knownDiscretizerClassesToReturn;
-    }
-
     public static Discretizer getDiscretizer(String attributeTargetName, Class discretizerClass) throws ServiceException {
         if (attributeTargetName == null) {
             throw new ServiceException("Attribute name cannot be null.");
+        } else if (attributeTargetName.isEmpty()){
+            throw new ServiceException("Attribute name cannot be empty.");
         }
-        if (discretizerClass.equals(NegativePositiveDiscretizer.class)) {
-            return new NegativePositiveDiscretizer(attributeTargetName);
+        
+        if ( !discretizerClass.isAssignableFrom(Discretizer.class)){
+            throw new ServiceException(discretizerClass.getCanonicalName() + " is not a "+ Discretizer.class.getCanonicalName() + " subclass!");
         }
-        if (discretizerClass.equals(DayOfWeekDiscretizer.class)) {
-            return new DayOfWeekDiscretizer(attributeTargetName);
-        }
-        if (discretizerClass.equals(HourOfDayDiscretizer.class)) {
-            return new HourOfDayDiscretizer(attributeTargetName);
-        }
-        if (discretizerClass.equals(RoundOfDayDiscretizer.class)) {
-            return new RoundOfDayDiscretizer(attributeTargetName);
-        }
-        if (discretizerClass.equals(NumberOfFilesDiscretizer.class)) {
-            return new NumberOfFilesDiscretizer(attributeTargetName);
-        }
-        if (discretizerClass.equals(DoubleDecimalCaseDiscretizer.class)) {
-            return new DoubleDecimalCaseDiscretizer(attributeTargetName);
-        }
-        throw new ServiceException("Unknown discretizer class: " + discretizerClass.toString());
-        //        if (!knownDiscretizerClasses.contains(discretizerClass)) {
-//            throw new ServiceException("Unknown discretizer class.");
-//        }
-//        try {
-//            Constructor c = discretizerClass.getConstructor(new Class[]{String.class});
-//
-//            return (Discretizer) c.newInstance(attributeTargetName);
-//        } catch (Exception ex) {
-//            throw new ServiceException(ex);
-//        }
+        try {
+            Discretizer newDiscretizer = (Discretizer)discretizerClass.newInstance();
+            newDiscretizer.setAttributeTarget(attributeTargetName);
+            return newDiscretizer;
+        } catch (Exception ex) {
+            throw new ServiceException(ex);
+        }          
     }
 }
