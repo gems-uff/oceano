7d12a1ea456f456b4dadded3f9d51b92e0753ae5
Daniel Heráclio <dheraclio@gmail.com>
2016-03-09 21:17:31 -0300

    Adding TravisCI config file, script for custom build and some dependencies not found at maven central

---
+++ travis.sh
@@ -0,0 +1,20 @@
+echo "Installing dependencies not found in maven central"
+
+echo "Installing Dependometer dependencies"
+mvn install:install-file -Dfile=dependencies/org.eclipse.cdt.core-5.3.1.201109151620.jar -DpomFile=dependencies/org.eclipse.cdt.core-5.3.1.201109151620.pom
+
+mvn install:install-file -Dfile=dependencies/dependometer-core-1.2.9.jar -DpomFile=dependencies/dependometer-core-1.2.9.pom
+
+mvn install:install-file -Dfile=dependencies/dependometer-cpp-1.2.9.jar -DpomFile=dependencies/dependometer-cpp-1.2.9.pom
+
+mvn install:install-file -Dfile=dependencies/dependometer-java-1.2.9.jar -DpomFile=dependencies/dependometer-java-1.2.9.pom
+
+mvn install:install-file -Dfile=dependencies/dependencyfinder-jar-1.2.3.jar -DpomFile=dependencies/dependencyfinder-jar-1.2.3.pom
+
+mvn install:install-file -Dfile=dependencies/ckjm-1.9.1.jar -DpomFile=dependencies/ckjm-1.9.1.pom
+
+
+echo "Building all modules"
+mvn -T 1C clean install -DskipTests -fae
+
+
